922f4e9466fc98b0ddc4b70ade6dcaa0f621c1a5
Tim Jonsson <4yourztruly@gmail.com>
2023-04-12 14:51:36 +0200

    JavaScript V1.03

--- main.js
+++ main.js
@@ -158,4 +158,119 @@ console.log(`first name: ${mike.firstName}`);
 let mikeStr = `${mike.firstName} ${mike.lastName}, age: ${mike.age}`;
 console.log(mikeStr);
 
+let mikeAdvanced = {
 
+    firstName: 'Mike',
+    lastName: 'Bäck',
+    age: 38,
+    hobbies: ['music', 'programmering', 'reading', 'games']
+};
+
+console.log(mikeAdvanced);
+let mikeJSON = JSON.stringify(mikeAdvanced); //object to JSON
+console.log(mikeJSON);
+console.log(JSON.parse(mikeJSON)); //back to object
+
+// loop over object
+for(let propertyName in mikeAdvanced) {
+
+    let propertyValue = mikeAdvanced [propertyName];
+    console.log(propertyName, propertyValue);
+};
+
+// connection to HTML
+//ask JS to create a new element
+let myDiv = document.createElement('div');
+// add some HTML
+myDiv.innerHTML = `
+    <h1>Hello from JS!</h1>
+    <p>This text is from JavaScript, my age is ${mike.age}</p>
+`;
+
+//grab the body tag
+let body = document.querySelector('body');
+
+//add the div to the HTML
+body.append(myDiv);
+
+//another way to add data
+let myBands = [
+{
+    name: 'The Beatles',
+    genre: 'Pop'
+},
+{
+    name: 'The Rolling Stones',
+    genre: 'Rock'
+}
+];
+
+//grab the bands div
+let bandsDiv = document.querySelector('#bands');
+for(let band of myBands) {
+
+    bandsDiv.innerHTML += `
+    <div class="band"> 
+        <h2>${band.name}</h2>
+        <p>${band.genre}</p>
+    </div>`
+    ;
+}
+
+//functions
+function sayHello() {
+
+    console.log('Hello');
+}
+const sayHello2 = () => {
+
+    console.log('Hello 2');
+}
+
+sayHello();
+sayHello2();
+
+//different way of declaring te same function
+
+function add (a, b) {
+
+    return a + b;
+}
+
+const add2 = (a, b) => {
+
+    return a + b;
+}
+
+const add3 = function(a, b) {
+
+    return a + b;
+}
+
+//arrow function with implicit return
+const add4 = (a, b) => a + b;
+
+//arrow function with implicit return and one paramater
+const add5 = a => a + 5;
+
+
+class Person {
+
+    constructor (name, age, hobby) {
+
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    greetings() {
+
+        console.log(`Hi, my name is ${this.name} and i am ${this.age}`);
+        console.log('I enjoy ' + this.hobby);
+
+    }
+}
+
+const mikeClass = new Person('Mike', 38, 'Music');
+mikeClass.greetings();
+console.log(mikeClass.hobby);
